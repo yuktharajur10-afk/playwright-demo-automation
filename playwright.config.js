f91cd8ef21fb5d67aa92315745e174f96e3e7e0c
yuktharajur10-afk <yuktharajur10@gmail.com>
2025-12-05 23:34:08 +0000

    Add Playwright automated tests project

---
+++ playwright.config.js
@@ -0,0 +1,7 @@
+use: {
+  headless: false,
+  viewport: { width: 1280, height: 720 },
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+  launchOptions: { slowMo: 500 }, // 500ms delay between actions
+},
